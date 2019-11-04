0490915ab9042c6d35047e59fc67f991883fe8f7
hytch <naofea9@gmail.com>
2019-11-05 01:14:57 +0900

    feature シークエント計算。ざっくりと論理的な理論に必要なのを実装。

---
+++ SequentCalculator/SequentCalculator/Sequent.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Elements;
+
+namespace SequentCalculator
+{
+    /// <summary>
+    /// シークエントクラス
+    /// </summary>
+    public class Sequent
+    {
+        /// <summary>
+        /// 仮定
+        /// </summary>
+        public List<CharSequence> Assumptions { get; private set; }
+        /// <summary>
+        /// 結論
+        /// </summary>
+        public CharSequence Conclusion { get; private set; }
+
+        public Sequent(List<CharSequence> assumptions, CharSequence conclusion)
+        {
+            foreach (var assumption in assumptions)
+            {
+                if (assumption.TermPropType != CharSequence.TermPropTypes.Proposition)
+                    throw new ArgumentException("命題以外が入力されました。");
+            }
+
+            if (conclusion.TermPropType != CharSequence.TermPropTypes.Proposition)
+                throw new ArgumentException("命題以外が入力されました。");
+
+            Assumptions = assumptions;
+            Conclusion = conclusion;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            if (Assumptions != null || Assumptions.Count != 0)
+            {
+                foreach (var assumption in Assumptions)
+                {
+                    sb.Append(assumption.ToString() + ",");
+                }
+
+                sb.Remove(sb.Length, 1);
+                sb.Append("→");
+            }
+
+            if (Conclusion != null)
+            {
+                sb.Append(Conclusion.ToString());
+            }
+            
+            return sb.ToString();
+        }
+    }
+}
---
+++ SequentCalculator/SequentCalculator/SequentCaluculator.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using Elements;
+using FormulaDetector;
+
+namespace SequentCalculator
+{
+    /// <summary>
+    /// シークエント計算用クラス
+    /// </summary>
+    public static class SequentCaluculator
+    {
+        /// <summary>
+        /// 仮定
+        /// </summary>
+        /// <param name="p">命題</param>
+        /// <param name="g">命題</param>
+        /// <returns>シークエント</returns>
+        public static Sequent UseAssuming(CharSequence g, CharSequence p)
+        {
+            return new Sequent(new List<CharSequence> { g, p }, p);
+        }
+
+        /// <summary>
+        /// 増
+        /// </summary>
+        /// <param name="s">シークエント</param>
+        /// <param name="p">命題</param>
+        /// <returns></returns>
+        public static Sequent UseAdding(Sequent s, CharSequence p)
+        {
+            s.Assumptions.Add(p);
+            return s;
+        }
+
+        /// <summary>
+        /// 減
+        /// </summary>
+        /// <param name="s">シークエント</param>
+        /// <returns></returns>
+        public static Sequent UseRemoving(Sequent s)
+        {
+            var deleteTargets = new List<int>();
+            for (var i = 0; i < s.Assumptions.Count; i++)
+            {
+                for (var j = i + 1; j < s.Assumptions.Count; j++)
+                {
+                    if (s.Assumptions[i].Equals(s.Assumptions[j]))
+                        if (!deleteTargets.Contains(j))
+                            deleteTargets.Add(j);
+                }
+            }
+
+            deleteTargets.Sort();
+            deleteTargets.Reverse();
+
+            foreach (var deleteTarget in deleteTargets)
+            {
+                s.Assumptions.RemoveAt(deleteTarget);
+            }
+
+            return s;
+        }
+
+        /// <summary>
+        /// 減
+        /// </summary>
+        /// <param name="s">シークエント</param>
+        /// <param name="i">交換インデックス</param>
+        /// <param name="j">交換インデックス</param>
+        /// <returns></returns>
+        public static Sequent UseExchanging(Sequent s, int i, int j)
+        {
+            if (i > s.Assumptions.Count)
+                throw new ArgumentException("不正なインデックスが指定されました");
+
+            if (j > s.Assumptions.Count)
+                throw new ArgumentException("不正なインデックスが指定されました");
+
+            var iSeq = s.Assumptions[i].Copy();
+            var jSeq = s.Assumptions[j].Copy();
+
+            s.Assumptions.RemoveAt(i);
+            s.Assumptions.Insert(i, jSeq);
+
+            s.Assumptions.RemoveAt(j);
+            s.Assumptions.Insert(j, iSeq);
+
+            return s;
+        }
+
+        /// <summary>
+        /// 切断
+        /// </summary>
+        /// <param name="s">シークエント</param>
+        /// <param name="t">シークエント(切断対象)</param>
+        /// <returns></returns>
+        public static Sequent UseCutting(Sequent s, Sequent t)
+        {
+            var cuttingTargets = new List<int>();
+            for (int i = 0; i < t.Assumptions.Count; i++)
+            {
+                if (t.Conclusion.Equals(s.Assumptions[i]))
+                    cuttingTargets.Add(i);
+            }
+
+            if (cuttingTargets.Count == 0)
+                return t;
+
+            cuttingTargets.Reverse();
+
+            foreach (var j in cuttingTargets)
+            {
+                t.Assumptions.RemoveAt(j);
+            }
+
+            return t;
+        }
+
+        /// <summary>
+        /// 代入
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="x"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static Sequent UseSubstitution(Sequent s, Charactor x, CharSequence t)
+        {
+            foreach (var seq in s.Assumptions)
+            {
+                seq.StashVariable(t, x);
+            }
+
+            s.Conclusion.StashVariable(t, x);
+
+            return s;
+        }
+    }
+}
